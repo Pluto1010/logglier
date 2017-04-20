22b2056d65eb46b1c0f92f3cc4796a2144aefc01
Dennis Schulz <d.schulz@ablida.de>
2017-04-20 16:03:27 +0200

    sry I forgot something :(

--- bulk.rb
+++ bulk.rb
@@ -5,7 +5,7 @@ require 'logglier'
 #url = "http://logs-01.loggly.com/bulk/TOKEN/tag/bulk/"
 url = "http://requestb.in/x4ue08x4"
 
-log = Logglier.new(, threaded: true, bulk: true, format: :json, bulk_max_size: 3)
+log = Logglier.new(url, threaded: true, bulk: true, format: :json, bulk_max_size: 3)
 
 10.times do |i|
   log.info({ hello: "world", i: i })
