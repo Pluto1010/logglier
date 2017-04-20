bb806d38a63f3a023f30addca4a9c7269eb02526
Dennis Schulz <d.schulz@ablida.de>
2017-04-20 16:02:26 +0200

    adds a URL template for loggly´s bulk endpoint to my simple test script.

--- bulk.rb
+++ bulk.rb
@@ -2,7 +2,10 @@ $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'logglier'
 
-log = Logglier.new('http://requestb.in/x4ue08x4', threaded: true, bulk: true, format: :json, bulk_max_size: 3)
+#url = "http://logs-01.loggly.com/bulk/TOKEN/tag/bulk/"
+url = "http://requestb.in/x4ue08x4"
+
+log = Logglier.new(, threaded: true, bulk: true, format: :json, bulk_max_size: 3)
 
 10.times do |i|
   log.info({ hello: "world", i: i })
